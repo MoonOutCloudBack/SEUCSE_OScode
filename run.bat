591f1e23bc3469f4bb51296022a5ea0732896476
JiXionghui <2467069057@qq.com>
2022-11-28 11:00:01 +0800

    替换boot.S

--- run.bat
+++ run.bat
@@ -1,2 +1,2 @@
 start /D "E:\OpenOCD" cmd /K openocd-nexys4.bat
-start "gdb" cmd /K "E:\MIPS\Toolchains\mips-mti-elf\2017.10-05\bin\mips-mti-elf-gdb.exe" -x startup.txt 
+start "gdb" cmd /K "E:\MipsMingw\bin\mips-mti-elf-gdb.exe" -x startup.txt 
